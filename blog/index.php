a3ff5b30a6fe214d990610198dddf87de299dd9c
Steven Berg <steven@stevenberg.net>
2019-04-11 13:15:39 -0400

    Initial commit

---
+++ blog/index.php
@@ -0,0 +1,3 @@
+<?php
+header ('Location: http://peiratikos.net/blog/archives/');
+?>
---
+++ wp-content/themes/peiratikos/scripts/peiratikos.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+	if (document.getElementById) {
+		if (document.getElementById('searchform')) {
+			var s = document.getElementById('s');
+			s.onfocus = function() {
+				if (this.value == 'Search Peiratikos') {
+					this.value = '';
+				}
+			}
+			s.onblur = function() {
+				if (this.value == '') {
+					this.value = 'Search Peiratikos';
+				}
+			}
+			document.getElementById('searchform').onsubmit = function() {
+				var stext = s.value.replace(/ /g, '+');
+				if (stext == '') {
+					stext = 'search';
+				}
+				//location.href = 'http://70.85.217.130/~steven/search/' + stext;
+				location.href = 'http://peiratikos.net/search/' + stext;
+				return false;
+			}
+		}
+	}
+}
